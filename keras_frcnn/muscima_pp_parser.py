2eae282d82df20fef44a14fa8006709b8824572b
Alexander Pacha <alexander.pacha@gmail.com>
2017-08-29 15:11:35 +0200

    Worked on muscima-parser

--- keras_frcnn/muscima_pp_parser.py
+++ keras_frcnn/muscima_pp_parser.py
@@ -1,60 +1,71 @@
+from glob import glob
 from typing import Tuple
 
 import cv2
+import numpy
 import numpy as np
+import re
+from PIL import Image
+import os
 from omrdatasettools.image_generators.MuscimaPlusPlusImageGenerator import MuscimaPlusPlusImageGenerator
+from muscima.cropobject import CropObject
 
 
-def get_data(image_path, raw_data_directory: str) -> Tuple[list, dict, dict]:
+def get_data(image_directory, raw_data_directory: str) -> Tuple[list, dict, dict]:
     all_imgs = {}
     classes_count = {}
     class_mapping = {}
     visualise = True
 
     image_generator = MuscimaPlusPlusImageGenerator()
-    all_xml_files = image_generator.get_all_xml_file_paths(raw_data_directory)[0:1]
-    crop_objects = image_generator.load_crop_objects_from_xml_files(all_xml_files)
+    all_xml_files = image_generator.get_all_xml_file_paths(raw_data_directory)#[0:1]
 
     print('Parsing annotation files')
 
-    filename = image_path
-    img = cv2.imread(filename)
+    for xml_file in all_xml_files:
+        crop_objects = image_generator.load_crop_objects_from_xml_files([xml_file])
+        doc = crop_objects[0].doc
+        result = re.match(r"CVC-MUSCIMA_W-(?P<writer>\d+)_N-(?P<page>\d+)_D-ideal", doc)
+        writer = result.group("writer")
+        page = result.group("page")
 
-    for crop_object in crop_objects:
-        class_name = crop_object.clsname
-        (x1, y1, x2, y2) = crop_object.bounding_box
+        #image_path = os.path.join(image_directory, "w-{0}".format(writer), "p0{0}.png".format(page))
+        image_path = os.path.join(image_directory, "w-{0}".format(writer),"image", "p0{0}.png".format(page))
 
-        if class_name not in classes_count:
-            classes_count[class_name] = 1
-        else:
-            classes_count[class_name] += 1
+        img = cv2.imread(image_path)
+        for crop_object in crop_objects:
+            class_name = crop_object.clsname
+            (top, left, bottom, right) = crop_object.bounding_box
+            x1, y1, x2, y2 = left, top, right, bottom
 
-        if class_name not in class_mapping:
-            class_mapping[class_name] = len(class_mapping)
-
-        if filename not in all_imgs:
-            all_imgs[filename] = {}
-
-            (rows, cols) = img.shape[:2]
-            all_imgs[filename]['filepath'] = filename
-            all_imgs[filename]['width'] = cols
-            all_imgs[filename]['height'] = rows
-            all_imgs[filename]['bboxes'] = []
-            if np.random.randint(0, 6) > 0:
-                all_imgs[filename]['imageset'] = 'trainval'
+            if class_name not in classes_count:
+                classes_count[class_name] = 1
             else:
-                all_imgs[filename]['imageset'] = 'test'
+                classes_count[class_name] += 1
 
-        all_imgs[filename]['bboxes'].append(
-            {'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2})
+            if class_name not in class_mapping:
+                class_mapping[class_name] = len(class_mapping)
+
+            if image_path not in all_imgs:
+                all_imgs[image_path] = {}
+
+                (rows, cols) = img.shape[:2]
+                all_imgs[image_path]['filepath'] = image_path
+                all_imgs[image_path]['width'] = cols
+                all_imgs[image_path]['height'] = rows
+                all_imgs[image_path]['bboxes'] = []
+                if np.random.randint(0, 6) > 0:
+                    all_imgs[image_path]['imageset'] = 'trainval'
+                else:
+                    all_imgs[image_path]['imageset'] = 'test'
+
+            all_imgs[image_path]['bboxes'].append({'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2})
+            if visualise:
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255))
 
         if visualise:
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255))
-            crop_object.mask
-
-    if visualise:
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
 
     all_data = []
     for key in all_imgs:
@@ -66,6 +77,6 @@ def get_data(image_path, raw_data_directory: str) -> Tuple[list, dict, dict]:
 if __name__ == "__main__":
     # get_data("annotationsample.txt", "../data/muscima_pp_raw")
     get_data(
-        "C:\\Users\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_SR\\CvcMuscima-Distortions\\ideal\\w-10\\image\\p010.png",
-        #"C:\\Users\\Alex\\Repositories\\MusicObjectDetector\\data\\CVCMUSCIMA_WI\\CVCMUSCIMA_WI\\PNG_GT_Gray\\w-01\\p010.png",
+        #"C:\\Users\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_WI\\CVCMUSCIMA_WI\\PNG_GT_Gray\\",
+        "C:\\Users\\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_SR\\CvcMuscima-Distortions\\ideal",
         "../data/muscima_pp_raw")
