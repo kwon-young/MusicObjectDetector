ee4850df263ed466c0dfe5ff8872787583098fd8
Alexander Pacha <alexander.pacha@gmail.com>
2017-08-30 15:52:26 +0200

    Completed first draft of muscima_pp_parser

--- keras_frcnn/muscima_pp_parser.py
+++ keras_frcnn/muscima_pp_parser.py
@@ -1,36 +1,30 @@
-from glob import glob
+import os
+import re
 from typing import Tuple
 
 import cv2
-import numpy
 import numpy as np
-import re
-from PIL import Image
-import os
 from omrdatasettools.image_generators.MuscimaPlusPlusImageGenerator import MuscimaPlusPlusImageGenerator
-from muscima.cropobject import CropObject
+from tqdm import tqdm
 
 
-def get_data(image_directory, raw_data_directory: str) -> Tuple[list, dict, dict]:
+def get_data(muscima_image_directory, muscima_pp_raw_data_directory: str, visualize=False) -> Tuple[list, dict, dict]:
     all_imgs = {}
     classes_count = {}
     class_mapping = {}
-    visualise = True
 
     image_generator = MuscimaPlusPlusImageGenerator()
-    all_xml_files = image_generator.get_all_xml_file_paths(raw_data_directory)#[0:1]
+    all_xml_files = image_generator.get_all_xml_file_paths(muscima_pp_raw_data_directory)
 
-    print('Parsing annotation files')
-
-    for xml_file in all_xml_files:
-        crop_objects = image_generator.load_crop_objects_from_xml_files([xml_file])
+    for xml_file in tqdm(all_xml_files, desc='Parsing annotation files'):
+        crop_objects = image_generator.load_crop_objects_from_xml_file(xml_file)
         doc = crop_objects[0].doc
         result = re.match(r"CVC-MUSCIMA_W-(?P<writer>\d+)_N-(?P<page>\d+)_D-ideal", doc)
         writer = result.group("writer")
         page = result.group("page")
 
-        #image_path = os.path.join(image_directory, "w-{0}".format(writer), "p0{0}.png".format(page))
-        image_path = os.path.join(image_directory, "w-{0}".format(writer),"image", "p0{0}.png".format(page))
+        # image_path = os.path.join(image_directory, "w-{0}".format(writer), "p0{0}.png".format(page))
+        image_path = os.path.join(muscima_image_directory, "CvcMuscima-Distortions", "ideal", "w-{0}".format(writer), "image", "p0{0}.png".format(page))
 
         img = cv2.imread(image_path)
         for crop_object in crop_objects:
@@ -60,10 +54,10 @@ def get_data(image_directory, raw_data_directory: str) -> Tuple[list, dict, dict
                     all_imgs[image_path]['imageset'] = 'test'
 
             all_imgs[image_path]['bboxes'].append({'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2})
-            if visualise:
+            if visualize:
                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255))
 
-        if visualise:
+        if visualize:
             cv2.imshow('img', img)
             cv2.waitKey(0)
 
@@ -75,8 +69,12 @@ def get_data(image_directory, raw_data_directory: str) -> Tuple[list, dict, dict
 
 
 if __name__ == "__main__":
-    # get_data("annotationsample.txt", "../data/muscima_pp_raw")
-    get_data(
-        #"C:\\Users\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_WI\\CVCMUSCIMA_WI\\PNG_GT_Gray\\",
-        "C:\\Users\\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_SR\\CvcMuscima-Distortions\\ideal",
-        "../data/muscima_pp_raw")
+    all_data, classes_count, class_mapping = get_data(
+        # "C:\\Users\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_WI\\CVCMUSCIMA_WI\\PNG_GT_Gray\\",
+        "C:\\Users\\\Alex\\Repositories\\CVC-MUSCIMA\\CVCMUSCIMA_SR",
+        "../data/muscima_pp_raw", False)
+
+    number_of_bounding_boxes = sum(classes_count.values())
+    print("Found {0} samples with {1} bounding-boxes belonging to {2} classes".format(len(all_data),
+                                                                                      number_of_bounding_boxes,
+                                                                                      len(classes_count)))
